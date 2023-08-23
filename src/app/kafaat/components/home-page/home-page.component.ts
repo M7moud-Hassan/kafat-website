2c06c4391c42754c427b682e8650b0460a73c8b1
Mahmoud Hassan <abosamour990@gmail.com>
2023-08-23 14:04:20 +0300

    merge

--- src/app/kafaat/components/home-page/home-page.component.ts
+++ src/app/kafaat/components/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component,Type } from '@angular/core';
+import { Component,ElementRef,Renderer2,Type,AfterViewInit } from '@angular/core';
 import { ItemCarousal } from '../carousal-home/carousal-home.component';
 import { DisplayContentComponent } from 'src/app/shared/components/display-content/display-content.component';
 
@@ -7,8 +7,16 @@ import { DisplayContentComponent } from 'src/app/shared/components/display-conte
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent {
+export class HomePageComponent implements AfterViewInit{
   showOverlay = false;
+  
+  constructor(private renderer: Renderer2, private el: ElementRef) {
+   
+    
+  }
+  ngAfterViewInit(): void {
+    this.playAnimation()
+  }
 
   openOrClosNav(value:boolean):void{
     this.showOverlay=value;
@@ -44,6 +52,20 @@ export class HomePageComponent {
 
   component: Type<any> = DisplayContentComponent;
 
-  
-  
+  playAnimation(){
+    const elements = this.el.nativeElement.querySelectorAll('.top-anim');
+    const elements2 = this.el.nativeElement.querySelectorAll('.top-30');
+    elements.forEach((element:any) => {
+      
+      this.renderer.setStyle(element, 'top', '0');
+    });
+
+    elements2.forEach((element:any) => {
+     if(window.innerWidth<720){
+      this.renderer.setStyle(element, 'top', '-40px');
+     }else{
+      this.renderer.setStyle(element, 'top', '30%');
+     }
+    });
+  }
 }
