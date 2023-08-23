52f6c5510cb42af9a38c616307c5c7b2dd8b2bd0
Mahmoud Hassan <abosamour990@gmail.com>
2023-08-23 20:32:35 +0300

    fix

--- src/app/shared/components/tabs-container/tabs-container.component.ts
+++ src/app/shared/components/tabs-container/tabs-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, HostListener, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-tabs-container',
@@ -12,4 +12,52 @@ export class TabsContainerComponent {
     this.indexSelected=index;
     this.tabSelected.emit(index);
   }
+  @ViewChild('targetElement', { static: true }) targetElement: ElementRef|undefined;
+  @ViewChild('nav', { static: true }) nav: ElementRef|undefined;
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(event: any) {
+    
+    const rect = this.targetElement!.nativeElement.getBoundingClientRect();
+    
+    if (rect.top <45) {
+      document.getElementById('navbar')!.style.display='none';
+      this.nav!.nativeElement.classList.add('fix-tabs');
+    } else {
+      document.getElementById('navbar')!.style.display='block';
+      this.nav!.nativeElement.classList.remove('fix-tabs');
+    }
+  }
+
+  slideConfig = {
+    "slidesToShow": 6,
+    "slidesToScroll": 1,
+    "infinite": false,
+    "responsive": [
+
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: false,
+
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      }
+    ]
+  };
 }
