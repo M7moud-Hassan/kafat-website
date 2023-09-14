586d8468dcb85613e497fb39e118fca6c736f989
Mahmoud Hassan <abosamour990@gmail.com>
2023-09-15 01:35:30 +0300

    merge

---
+++ src/app/dashboard/components/add-report/add-report.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-report',
+  templateUrl: './add-report.component.html',
+  styleUrls: ['./add-report.component.css']
+})
+export class AddReportComponent{
+}
