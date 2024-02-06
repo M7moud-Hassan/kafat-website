063eae3079bbeaa317f96f5ef7ad61f2e83401a0
Mahmoud Hassan <abosamour990@gmail.com>
2024-02-07 00:14:20 +0200

    merge

--- src/app/kafaat/components/header/header.component.ts
+++ src/app/kafaat/components/header/header.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, Input } from '@angular/core';
 
 @Component({
@@ -7,4 +8,29 @@ import { Component, Input } from '@angular/core';
 })
 export class HeaderComponent {
   @Input() imgPath='';
+constructor(private http: HttpClient) {
+  
+}
+
+  async imageToBase64(imageUrl: string): Promise<string> {
+    try {
+      const response = await this.http.get(imageUrl, { responseType: 'arraybuffer' }).toPromise();
+      const bytes = new Uint8Array(response);
+      const base64String = btoa(String.fromCharCode(...bytes));
+      return base64String;
+    } catch (error) {
+      console.error('Failed to load image', error);
+      throw error;
+    }
+  }
+
+  async loadImageAndConvertToBase64() {
+
+    try {
+      const base64String = await this.imageToBase64(this.imgPath);
+      console.log('Base64 encoded image:', base64String);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
 }
